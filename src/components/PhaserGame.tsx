458711a702a3fc0ef2663d52093fd14e912c2b2d
stckrz <rin449@gmail.com>
2024-10-10 22:22:20 -0500

    push to desktop

--- src/components/PhaserGame.tsx
+++ src/components/PhaserGame.tsx
@@ -1,7 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Phaser from 'phaser';
+import styles from './phaserGame.module.css';
+
+interface MyScene extends Phaser.Scene {
+	cursors: Phaser.Types.Input.Keyboard.CursorKeys
+	spacebar: Phaser.Input.Keyboard.Key,
+	right: Phaser.Input.Keyboard.Key,
+}
 
 const PhaserGame: React.FC = () => {
+	const [playingAnimation, setPlayingAnimation] = useState("idle")
+	let corgo;
+	let corgoDirection = "right";
 	useEffect(() => {
 		const config: Phaser.Types.Core.GameConfig = {
 			type: Phaser.AUTO,
@@ -23,7 +33,6 @@ const PhaserGame: React.FC = () => {
 
 		const game = new Phaser.Game(config);
 		function preload(this: Phaser.Scene) {
-			// this.load.image('corgo', '/sprites/corgi-assetnotail.png')
 			this.load.spritesheet('corgo', '/sprites/corgi-assetnotail.png', {
 				frameWidth: 62,
 				frameHeight: 64,
@@ -31,64 +40,111 @@ const PhaserGame: React.FC = () => {
 				spacing: 2
 			});
 		}
-
-		function create(this: Phaser.Scene) {
-			const corgo = this.physics.add.sprite(200, 100, 'corgo');
+		function create(this: MyScene) {
+			this.cursors = this.input.keyboard.createCursorKeys();
+			corgo = this.physics.add.sprite(400, 300, 'corgo');
 			corgo.setScale(2)
-			corgo.body.setAllowGravity(false)
-			this.anims.create({
-				key: 'jump',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 1, end: 12}),
-				frameRate: 10,
-				repeat: -1
-			});
-			this.anims.create({
-				key: 'idle',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 26, end: 30}),
-				frameRate: 10,
-				repeat: -1
-			});
-			this.anims.create({
-				key: 'sit',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 38, end: 46}),
-				frameRate: 10,
-				repeat: -1
-			});
-			this.anims.create({
-				key: 'walk',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 50, end: 54}),
-				frameRate: 10,
-				repeat: -1
-			});
-			this.anims.create({
-				key: 'run',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 62, end: 69}),
-				frameRate: 10,
-				repeat: -1
-			});
-			this.anims.create({
-				key: 'sniff',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 73, end: 80}),
-				frameRate: 10,
-				repeat: -1
-			});
-			this.anims.create({
-				key: 'sniffwalk',
-				frames: this.anims.generateFrameNumbers('corgo', {start: 85, end: 92}),
-				frameRate: 10,
-				repeat: -1
-			});
-			corgo.anims.play('sniffwalk')
+			corgo.body.setAllowGravity(true)
+			corgo.setCollideWorldBounds(true);
+
+			const create_animation = (
+				scene: Phaser.Scene,
+				key: string,
+				start: number,
+				end: number
+			) => {
+				scene.anims.create({
+					key: key,
+					frames: scene.anims.generateFrameNumbers('corgo', { start: start, end: end }),
+					frameRate: 10,
+					repeat: -1
+				})
+			}
+			this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+			this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+			create_animation(this, 'jump', 2, 11)
+			create_animation(this, 'idle', 26, 30)
+			create_animation(this, 'sit', 38, 46)
+			create_animation(this, 'walk', 50, 54)
+			create_animation(this, 'run', 62, 69)
+			create_animation(this, 'sniff', 74, 80)
+			create_animation(this, 'sniffwalk', 86, 92)
+
+			corgo.anims.play(playingAnimation)
 		}
 
-		function update() { }
+		function update(this: MyScene) {
+			corgo.setVelocityX(0)
+
+			if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
+				corgo.setFlipX(true);
+				corgo.anims.play('walk')
+			}
+			if (Phaser.Input.Keyboard.JustUp(this.cursors.left)) {
+				corgo.anims.play('idle')
+			}
+
+			if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
+				corgo.setFlipX(false);
+				corgo.anims.play('walk')
+			}
+			if (Phaser.Input.Keyboard.JustUp(this.cursors.right)) {
+				corgo.anims.play('idle')
+			}
+			if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+				console.log('space pressed')
+			}
+
+			if (this.cursors.left.isDown) {
+				corgo.setVelocityX(-160);
+				corgoDirection = "left"
+			}
+			if (this.cursors.right.isDown) {
+				corgo.setVelocityX(160);
+				corgoDirection = "right"
+			}
+
+			// if (this.cursors.up.isDown && !isJumping) {
+			// 	isJumping = true;
+			// 	jumpTimer = 0;
+			// }
+			// if (isJumping && jumpTimer <= 10) {
+			//
+			// 	let velocity = 2000
+			// 	velocity = velocity / (jumpTimer + 1)
+			// 	corgo.setVelocityY(-velocity);
+			//
+			// 	jumpTimer++
+			// }
+			//
+			// if (corgo.body.touching.down) {
+			// 	isJumping = false;
+			// 	jumpTimer = 0;
+			// }
+
+			if (this.cursors.down.isDown) {
+				corgoDirection = "down"
+			}
+		}
 
 		return () => {
 			game.destroy(true);
 		};
-	}, []);
+	}, [playingAnimation]);
+	console.log(playingAnimation)
 
-	return <div id="phaser-game"></div>;
+	return <div className={styles.gameBox}>
+		<div id="phaser-game"></div>
+		<div className={styles.buttonBox}>
+			<button onClick={() => { setPlayingAnimation("jump") }}>jump</button>
+			<button onClick={() => { setPlayingAnimation("idle") }}>idle</button>
+			<button onClick={() => { setPlayingAnimation("sit") }}>sit</button>
+			<button onClick={() => { setPlayingAnimation("walk") }}>walk</button>
+			<button onClick={() => { setPlayingAnimation("run") }}>run</button>
+			<button onClick={() => { setPlayingAnimation("sniff") }}>sniff</button>
+			<button onClick={() => { setPlayingAnimation("sniffwalk") }}>sniffwalk</button>
+		</div>
+	</div>;
 };
 
 export default PhaserGame;
